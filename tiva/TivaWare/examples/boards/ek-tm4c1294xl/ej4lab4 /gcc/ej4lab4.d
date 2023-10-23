1200d6f43187f60c8e36cacac65f609f5b0df90a
97hackbrian <brayandurantoconas@gmail.com>
2023-10-22 20:15:29 -0400

    errorGit

--- tiva/TivaWare/examples/boards/ek-tm4c1294xl/ej4lab4 /gcc/ej4lab4.d
+++
@@ -1,7 +0,0 @@
-gcc/ej4lab4.o: ej4lab4.c \
- /usr/lib/gcc/arm-none-eabi/12.2.1/include/stdint.h \
- /usr/lib/gcc/arm-none-eabi/12.2.1/include/stdbool.h \
- ../../../../inc/hw_memmap.h ../../../../driverlib/debug.h \
- ../../../../driverlib/gpio.h ../../../../driverlib/sysctl.h \
- ../../../../driverlib/interrupt.h ../../../../driverlib/timer.h \
- ../../../../inc/tm4c1294ncpdt.h
--- tiva/TivaWare/examples/boards/ek-tm4c1294xl/ej4lab4 /gcc/startup_gcc.d
+++
@@ -1,3 +0,0 @@
-gcc/startup_gcc.o: startup_gcc.c \
- /usr/lib/gcc/arm-none-eabi/12.2.1/include/stdint.h \
- ../../../../inc/hw_nvic.h ../../../../inc/hw_types.h
