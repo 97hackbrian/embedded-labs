7edea85b3cb79b0866653c32e2e40dd3419ca1c3
97hackbrian <brayandurantoconas@gmail.com>
2023-10-25 21:54:30 -0400

    unirLAptop

--- Raspberry/lab8/pixie.py
+++ Raspberry/lab8/pixie.py
@@ -164,15 +164,11 @@ class img(img_abs):
     def retorno(self):
         return self.imagen
     
-    def Cannycontours(self,u1,u2,b1,b2,retorno):
-        img=self.convIMGgray(1)
+    def Cannycontours(self,u1,u2,b1,b2):
+        img=self.convIMGgray()
         img = cv2.GaussianBlur(self.imagen,(b1,b2),0)
         Canny = cv2.Canny(img, u1, u2)
-
-        if retorno==1:
-            return Canny
-        else:
-            self.imagen= Canny
+        return Canny
     
     def apply_binary_threshold(self, threshold_value):
         _, binary_threshold = cv2.threshold(self.imagen, threshold_value, 255, cv2.THRESH_BINARY)
