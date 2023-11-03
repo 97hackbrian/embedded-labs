75e304a738f3df7bda4bcd1c6c7bb12c8b694810
97hackbrian <brayandurantoconas@gmail.com>
2023-11-03 01:31:20 -0400

    TivaFramework

--- tankbot/libs/tiva.py
+++ tankbot/libs/tiva.py
@@ -1,5 +1,5 @@
 import serial as com
-
+from time import sleep
 class InitSerial:
     def __init__(self,baud) -> None:
         for x in range(11):
@@ -19,7 +19,42 @@ class InitSerial:
                 
         
 class Motors:
-    def __init__(self,number_motors,serial_instance) -> None:
-        self.ser=serial_instance
-    def move(self,left,right):
-        self.ser.send_data("",,)
+    num_motor = 1  # Variable de clase para llevar un registro del número de instancias
+    
+    def __init__(self, serial_instance) -> None:
+        self.ser = serial_instance
+        self.motor_number = Motors.num_motor  # Asignar el número de motor automáticamente
+        Motors.num_motor += 1  # Incrementar el número de instancias
+
+    def move(self, left, right):
+        self.ser.send_data(f"motor{self.motor_number}", left, right)
+
+    def stop(self):
+        self.ser.send_data(f"motor{self.motor_number}", -1, -1)
+
+class LedControl:
+    def __init__(self, serial_instance) -> None:
+        self.ser = serial_instance
+
+    def write(self, l1, l2, l3, l4):
+        # Verifica si los valores son booleanos
+        if not all(isinstance(val, bool) for val in (l1, l2, l3, l4)):
+            raise ValueError("Los valores deben ser booleanos")
+        self.ser.send_data(f"leds", l1, l2, l3, l4)
+
+    def init_system(self,cam=0):
+        sequences = [(1, 0, 0, 0), (0, 1, 0, 0), (0, 0, 1, 0), (0, 0, 0, 1), (1, 1, 1, 1), (1, 1, 1, 1),(0, 0, 0,01)]
+
+        for sequence in sequences:
+            try:
+                self.write(*sequence)
+                if self.ser.is_open:
+                    print(".", end="", flush=True)  # Establece end="" para evitar el salto de línea
+                if cam != 0:
+                    if cam.is_open():  # Comprueba si la cámara está abierta
+                        print("Cámara de la clase Camara está conectada.")
+                    else:
+                        print("Cámara de la clase Camara no está conectada.")
+            except:
+                print("Problem to connect to Serial")
+            sleep(0.35)
--- tankbot/scr/test.py
+++ tankbot/scr/test.py
@@ -1,3 +1,18 @@
-import serial
-import time
-import cv2
+from time import sleep
+from libs.tiva import *
+
+
+if __name__ == "__main__":
+    tiva1=InitSerial(baud=9600)
+    motors=Motors(serial_instance=tiva1)
+    Leds=LedControl(serial_instance=tiva1)
+    Leds.init_system()
+    motors.stop()
+    motors.move(50,50)
+    sleep(3)
+    motors.move(100,100)
+    sleep(3)
+    motors.stop()
+    sleep(5)
+    motors.move(-100,100)
+    sleep(3)
