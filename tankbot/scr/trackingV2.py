691e8207584e8d473488c467e2dd57a2a294177e
97hackbrian <brayandurantoconas@gmail.com>
2023-11-09 13:09:40 -0400

    DETECION FUNCIONA

--- tankbot/scr/trackingV2.py
+++ tankbot/scr/trackingV2.py
@@ -16,10 +16,11 @@ cap = cv2.VideoCapture(0)
 
 # Object detection from Stable camera
 object_detector = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=40)
-clahe=cv2.createCLAHE(clipLimit=2, tileGridSize=(27,27))
+clahe=cv2.createCLAHE(clipLimit=30, tileGridSize=(57,57))
 while True:
     ret, frame = cap.read()
-    frame=frame[:,:,2]
+    frame=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+    frame=frame[:,:,0]
     frame = clahe.apply(frame)
     #frame=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     frame=cv2.GaussianBlur(frame,(23,23),0)
@@ -27,26 +28,26 @@ while True:
     height, width = frame.shape
 
     # Extract Region of interest
-    roi = frame[100:,: ]
+    roi = frame[380:,: ]
 
     # 1. Object Detection
     mask = object_detector.apply(roi)
    #_, mask = cv2.threshold(mask, 254, 255, cv2.THRESH_BINARY)
     mask = cv2.adaptiveThreshold(mask, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 11, 40)
-    mask = cv2.Canny(mask,200,250)
+    mask = cv2.Canny(mask,10,1)
     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     detections = []
     for cnt in contours:
         # Calculate area and remove small elements
         area = cv2.contourArea(cnt)
-        if area <2000 and area>40:
+        if area <1000 and area>100:
             #cv2.drawContours(roi, [cnt], -1, (0, 255, 0), 2)
             x, y, w, h = cv2.boundingRect(cnt)
-            if(x>390):
+            if(x>400):
                 print("derecha")
                 motors.move(70,-70)
                 
-            elif(x<240):
+            elif(x<230):
                 motors.move(-80,80)
                 print("izquierda")
             else:
