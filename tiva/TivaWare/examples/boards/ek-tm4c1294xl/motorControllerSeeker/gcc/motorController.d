0e404c29c3fd57f1bc974c4506362cbb274a16fe
97hackbrian <brayandurantoconas@gmail.com>
2023-11-16 15:16:32 -0400

    MotorSeeker

---
+++ tiva/TivaWare/examples/boards/ek-tm4c1294xl/motorControllerSeeker/gcc/motorController.d
@@ -0,0 +1,26 @@
+gcc/motorController.o: motorController.c /usr/include/newlib/stdio.h \
+ /usr/include/newlib/_ansi.h /usr/include/newlib/newlib.h \
+ /usr/include/newlib/_newlib_version.h /usr/include/newlib/sys/config.h \
+ /usr/include/newlib/machine/ieeefp.h /usr/include/newlib/sys/features.h \
+ /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/lib/gcc/arm-none-eabi/12.2.1/include/stddef.h \
+ /usr/lib/gcc/arm-none-eabi/12.2.1/include/stdarg.h \
+ /usr/include/newlib/sys/reent.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/sys/_types.h /usr/include/newlib/machine/_types.h \
+ /usr/include/newlib/sys/lock.h /usr/include/newlib/sys/types.h \
+ /usr/include/newlib/sys/_stdint.h \
+ /usr/include/newlib/sys/_pthreadtypes.h \
+ /usr/include/newlib/machine/types.h /usr/include/newlib/sys/stdio.h \
+ /usr/lib/gcc/arm-none-eabi/12.2.1/include/stdint.h \
+ /usr/lib/gcc/arm-none-eabi/12.2.1/include/stdbool.h \
+ ../../../../inc/hw_memmap.h ../../../../driverlib/debug.h \
+ ../../../../driverlib/gpio.h ../../../../driverlib/sysctl.h \
+ ../../../../driverlib/pin_map.h ../../../../driverlib/rom.h \
+ ../../../../driverlib/rom_map.h ../../../../inc/hw_ints.h \
+ ../../../../driverlib/interrupt.h ../../../../driverlib/timer.h \
+ ../../../../inc/tm4c1294ncpdt.h ../../../../driverlib/uart.h \
+ ../../../../utils/uartstdio.c ../../../../inc/hw_types.h \
+ ../../../../inc/hw_uart.h ../../../../utils/uartstdio.h \
+ /usr/include/newlib/string.h /usr/include/newlib/sys/string.h \
+ ../../../../driverlib/pwm.h
